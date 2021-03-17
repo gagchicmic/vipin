90af07219cb29e1eab49ead1fcc4b7d6c5d80ffe
GagChicmic <gagchicmic@gmail.com>
2021-03-17 22:08:37 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Form from "./Form";
 const App = () => {
   const loginFields = [
     { email: { type: "email" } },
-    { password: { type: "password" } },
+    { passwords: { type: "password" } },
   ];
   return <Form fields={loginFields} />;
 };
